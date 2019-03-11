f8b720dd518d4b377c1966f6de79b975db379d74
sunqishun <452750483@qq.com>
2019-03-11 20:21:19 +0800

    second commit

--- Java/src/main/java/com/contest/GildedRose.java
+++ Java/src/main/java/com/contest/GildedRose.java
@@ -1,14 +1,53 @@
 package com.contest;
-
+ 
 class GildedRose {
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
-    public void updateQuality() {
-        items[0].sellIn = 9;
-        items[0].quality = 19;
+    /**
+     * 商品销售期和品质计算方法
+     * @param name
+     * @param sellIn
+     * @param quality
+     */
+    public void updateQuality(String name,int sellIn,int quality) {
+    	if("Normal".equals(name)){//普通商品
+    		items[0].sellIn = sellIn-1;
+            items[0].quality = quality-1;
+            if(sellIn < 0){//商品过了销售期后，品质每天减2
+            	items[0].quality = quality-2;
+            }
+            if(quality < 0){//品质为负数时，设为0
+            	items[0].quality = 0;
+            }
+    	}else if("Aged Brie".equals(name)){//法国干酪
+    		items[1].sellIn = sellIn-1;
+            items[1].quality = quality+1;
+            if(quality > 50){//品质不会大于50
+            	items[0].quality = 50;
+            }
+    	}else if("Sulfuras".equals(name)){//传奇商品
+    		items[2].sellIn = sellIn;
+            items[2].quality = 80;
+    	}else if("Backstage passes".equals(name)){//剧场后台通行证
+    		if(sellIn>10){
+    			items[3].sellIn = sellIn-1;
+                items[3].quality = quality+1;
+    		}
+    		if(sellIn>5 && sellIn<10){
+    			items[3].sellIn = sellIn-1;
+                items[3].quality = quality+2;
+    		}
+    		if(sellIn>0 && sellIn<5){
+    			items[3].sellIn = sellIn-1;
+                items[3].quality = quality+3;
+    		}
+    		if(sellIn<0){
+    			items[3].sellIn = sellIn;
+                items[3].quality = 0;
+    		}
+    	}
     }
 }
--- Java/src/test/java/com/contest/GildedRoseTest.java
+++ Java/src/test/java/com/contest/GildedRoseTest.java
@@ -8,10 +8,12 @@ public class GildedRoseTest {
 
     @Test
     public void should_fix_me() {
-        Item[] items = new Item[] { new Item("normal", 10, 20) };
+        Item[] items = new Item[] { new Item("Normal", 10, 20),new Item("Aged Brie", 10, 20),new Item("Sulfuras", 10, 80),new Item("Backstage passes", 10, 20) };
         GildedRose gildedRose = new GildedRose(items);
         
-        gildedRose.updateQuality();
+        for(Item item : items){
+        	gildedRose.updateQuality(item.name,item.sellIn,item.quality);
+        }
 
         assertEquals(9, (gildedRose.items[0]).sellIn);
         assertEquals(19, gildedRose.items[0].quality);
